253989fe9ed0bd08b825d4f5765d10ea5169e66e
Jojohung <nsdrum@hotmail.com>
2023-05-24 11:15:16 +0800

    update-contents

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace LambdaExpression
     {
         static void Main(string[] args)
         {
+            int a;
+        }
+
+        static int myMethod(string message)
+        {
+            Console.WriteLine(message);
+            return 0;
         }
     }
 }
