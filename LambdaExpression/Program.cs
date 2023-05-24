68a365c93405ec46df442c7a6b8454bdfbdc82e9
Jojohung <nsdrum@hotmail.com>
2023-05-24 12:29:16 +0800

    update contents

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -11,7 +11,17 @@ namespace LambdaExpression
     {
         static void Main(string[] args)
         {
-            int a;
+            Func<int, int, int> sum = delegate (int a, int b) { return a + b; };
+            Console.WriteLine(sum(3,4));
+
+            // Impliment the Lambda Expression
+            Func<int, int, int> sum_2 = (a, b) => a + b;
+            Console.WriteLine(sum_2(5,6));
+
+            Func<int> str = () => myMethod("Hello");
+            Console.WriteLine(str());
+
+            Console.ReadLine();
         }
 
         static int myMethod(string message)
